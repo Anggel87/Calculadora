d1b1e4ecb83b490639a1d4482fadcd3abcbf3fc0
Anggel87 <angelheriberto87@gmail.com>
2024-09-17 21:25:59 -0600

    [ADD] - Diseño y onClick básico

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.calculadora;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCA, btnC, btnPorcentaje, btnEntre, btnPor, btnMenos, btnMas, btnIgual, btnPunto, btn1, btn2,
+    btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,37 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnCA = (Button) findViewById(R.id.btnCA);
+        btnC = (Button) findViewById(R.id.btnC);
+        btnPorcentaje = (Button) findViewById(R.id.btnPorcentaje);
+        btnEntre = (Button) findViewById(R.id.btnEntre);
+        btnPor = (Button) findViewById(R.id.btnPor);
+        btnMenos = (Button) findViewById(R.id.btnMenos);
+        btnMas = (Button) findViewById(R.id.btnMas);
+        btnIgual = (Button) findViewById(R.id.btnIgual);
+        btnPunto = (Button) findViewById(R.id.btnPunto);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn8 = (Button) findViewById(R.id.btn8);
+        btn9 = (Button) findViewById(R.id.btn9);
+        btn0 = (Button) findViewById(R.id.btn0);
+
+        btnCA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Hermano realmente es un papoi", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    public void BotonPrueba(View view){
+        Toast.makeText(this,"Otra forma de hacerlo xdxdxd",Toast.LENGTH_SHORT).show();
+    }
+
 }
